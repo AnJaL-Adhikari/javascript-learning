9d5e46bd46d6c1b2c0fe6d48164f36e777db6025
AnJaL Adhikari <anjaladhikari01@gmail,com>
2026-07-14 22:45:57 +0545

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 143439;
+let accountEmail = "anjaladhikari01@gmail.com";
+var accountPassword = "12234";
+accountCity = "Dharan";
+accountEmail = 'anjaldotcom'
+accountPassword = "123321"
+let accountState;
+console.log(accountCity);
+
+// accountId = 2; Not allowed
+/* Prefer not to use var because of issue in block scope and functional scope */
+
+
+console.table([accountCity, accountId, accountPassword, accountEmail, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello"); we are using nodejs, not browser
+
+let name = "Anjal"
+let age = 22
+let isLoggedIn = true
+
+//number => 2 to the power 53
+//bigInt 
+//String => ""
+//boolen => true/false
+//null => standalone value
+//undefined =>
+// symbol => unique 
+
+
+//object 
+
+console.log(typeof null);  //object
+console.log(typeof undefined);  //undefined
+
