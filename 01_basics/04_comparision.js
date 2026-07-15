b289adf505a55f26ff3fb88c40e526e7dcae3073
AnJaL Adhikari <anjaladhikari01@gmail,com>
2026-07-15 11:11:25 +0545

    comparision of diff numbers

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+console.log(2 > 1)
+console.log(2 >= 1)
+console.log(2 < 1)
+console.log(2 ==1)
+console.log(2 != 1)
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+console.log("2" == 2) //true == conversion exist 
+console.log("2" === 2) //false === checks both value and datatype
+
