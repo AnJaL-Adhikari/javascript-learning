1795eb62bd84926f4081359aafe32d3e9c725d98
AnJaL Adhikari <anjaladhikari01@gmail,com>
2026-07-15 16:15:42 +0545

    Change in location of file

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,56 @@
+// Primitive datatypes
+
+// 7 types : String , Number , Boolean, null, undefined, Symbol , BigInt
+
+
+
+// JavaScript is dynamically typed because
+// i) Variables do not have fixed types.
+// ii) The type is determined at runtime based on the value assigned.
+// iii) A variable can hold values of different types over its lifetime.
+
+
+const score = 100
+const scoreValue = 100.3
+ const isLoggedIn = false
+ const outsideTemp = null
+
+
+//  let userEmail;
+//  let useEmail = undefined  // both assigns the undefined value
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3874387438743748374837483748n
+console.log(bigNumber)
+
+
+
+// Reference Type (Non primitive datatypes)
+
+// Array, Objects , Functions 
+
+
+const heros = ["ironman" , "thor" , "hulk", "doctorstrange"] //Array
+let myObj =           //Object
+{  
+    name : 'spiderman',
+    age : 22
+}
+
+console.log(heros)
+console.log(myObj.name)
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+
+console.log(typeof myObj)
+console.log(typeof outsideTemp) // data type of null returns object 
+console.log(typeof myFunction)
+
+// Link to study : https://262.ecma-international.org/5.1/#sec-11.4.3
